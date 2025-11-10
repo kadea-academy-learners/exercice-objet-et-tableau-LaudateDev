ed06f5a17b817cf073291463326bea56a3ae6ec8
Beni Tasisu <emmanuel.tasisu@gmail.com>
2025-11-10 17:53:07 +0100

    Suite et fin

--- 05-authentification-system.js
+++ 05-authentification-system.js
@@ -21,11 +21,51 @@
 const baseDeDonnees = [];
 
 function signUp(nom, email, password, confirmPassword) {
-	
+  for (let i = 0; i < baseDeDonnees.length; i++) {
+    if (baseDeDonnees[i].email === email) {
+      return "Cet email existe déjà !";
+    }
+  }
+
+  if (password !== confirmPassword) {
+    return "Les mots de passe ne correspondent pas !";
+  }
+
+  let nouvelId = baseDeDonnees.length + 1;
+
+  let nouvelUtilisateur = {
+    id: nouvelId,
+    nom: nom,
+    email: email,
+    password: password,
+    estConnecte: false,
+    estBloque: false,
+  };
+
+  baseDeDonnees.push(nouvelUtilisateur);
+
+  return nouvelUtilisateur;
 }
 
-function login() {
-	
+function login(email, password) {
+  for (let i = 0; i < baseDeDonnees.length; i++) {
+    let utilisateur = baseDeDonnees[i];
+
+    if (utilisateur.email === email) {
+      if (utilisateur.password !== password) {
+        return "Mot de passe incorrect !";
+      }
+
+      if (utilisateur.estBloque === true) {
+        return "Votre compte est bloqué !";
+      }
+
+      utilisateur.estConnecte = true;
+      return utilisateur;
+    }
+  }
+
+  return "Utilisateur introuvable !";
 }
 
 module.exports = { baseDeDonnees, signUp, login };
